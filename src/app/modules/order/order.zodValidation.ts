7cf559fcfa01516575389a5063a552d211f53e98
sharifalmahmudjamil <smjamil111@gmail.com>
2024-05-22 19:01:45 -1200

    order data get successfully

--- src/app/modules/order/order.zodValidation.ts
+++ src/app/modules/order/order.zodValidation.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
 const OrderValidationSchema = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
+  email: z.string().email({
+    message: 'Invalid email address, please enter valid email address',
+  }),
   productId: z.string({ required_error: 'Product ID is required' }),
   price: z
     .number({ required_error: 'Price is required' })
-    .positive({ message: 'Price must be a positive number' }),
+    .positive({ message: 'Price must be use  positive number' }),
   quantity: z
     .number({ required_error: 'Quantity is required' })
     .int({ message: 'Quantity must be an integer' })
